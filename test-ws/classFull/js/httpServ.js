a0351f4405cbf15e45b84b5428bc919da5b16be5
SimonGendre <135613008+SimonGendre@users.noreply.github.com>
2023-06-06 16:40:56 +0200

    code plus propre et séparé en classe

---
+++ test-ws/classFull/js/httpServ.js
@@ -0,0 +1,60 @@
+class HttpServ {
+  constructor() {}
+  /**
+   * cette méthode permet de récuperer l'ip du visiteur et de l'afficher
+   */
+  getIP() {
+    let ip = "";
+    ipGet.getIP((data) => {
+      ip = data.ip;
+    });
+    return ip;
+  }
+  /**
+   *  cette méthode permet de récuperer les coordonées GPS du visiteur et de les afficher
+   * return coordonnes GPS "lon:XXX,lat:YYY"
+   */
+  getLocalisation(ip) {
+    let api = "";
+    ipGet.getLocalisation(ip, (data) => {
+      api = data;
+    });
+    let retour = api.lon + "," + api.lat;
+    return retour;
+  }
+  /**
+   * cette méthode récupert le Json stocké dans un api et le returne
+   */
+  getStoredData() {
+    let donnesDistantes = {};
+    rmtData.getJson((data) => {
+      donnesDistantes = data;
+    });
+    return JSON.parse(donnesDistantes);
+  }
+  /**
+   * cette méthode ajoute une ligne au json en ligne
+   * la ligne est :
+   * ,MMJJHHmm : {lon : XXX, lat : YYY}
+   */
+  addData(lon, lat) {
+    //creation de la ligne à ajouté
+    let date = new Date();
+    let header =
+      date.getMonth() +
+      "" +
+      date.getDate() +
+      date.getHours() +
+      date.getMinutes();
+    let value = "lon:" + lon + ", lat:" + lat + "";
+    console.log(value);
+
+    //ajout de la ligne
+    let data = this.getStoredData();
+    data[header] = value;
+    console.log(data);
+
+    //ecriture en ligne
+    rmtData.setJson(JSON.stringify(data), () => {});
+  }
+}
---
+++ test-ws/classFull/js/ipGetter.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+class ipGetter {
+  constructor() {}
+  /**
+   * cette méthode récupert l'Ip du client
+   */
+  getIP(successCallback) {
+    let url = "https://jsonip.com/";
+
+    // envoi de la requête
+    $.ajax(url, {
+      type: "GET",
+      contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+      async: false,
+      success: successCallback
+      
+    });
+  }
+  /**
+   * cette méthode permet de récuperer l'emplacement d'une Ip passée en paramètre
+   */
+  getLocalisation(ip, successCallback) {
+    let url = "http://ip-api.com/json/" + ip;
+
+    // envoi de la requête
+    $.ajax(url, {
+      type: "GET",
+      contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+      async: false,
+      success: successCallback
+      
+    });
+  }
+}
